791d9d34e2dab723bcf7254e21e794e099a673e1
Aires Gonçalves <a.silva.goncalves@avanade.com>
2023-05-05 20:48:22 -0300

    page ticket form

--- src/app/pages/ticket/ticket-rounting.module.ts
+++ src/app/pages/ticket/ticket-rounting.module.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from "@angular/router";
 import { TicketListComponent } from "./ticket-list/ticket-list.component";
 import { NgModule } from "@angular/core";
+import { TicketFormComponent } from "./ticket-form/ticket-form.component";
 
 const routes: Routes = [
     { path: '', component: TicketListComponent },
+    { path: 'novo', component: TicketFormComponent },
 ];
 
 @NgModule({
--- src/app/pages/ticket/ticket.module.ts
+++ src/app/pages/ticket/ticket.module.ts
@@ -1,21 +1,25 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { TicketApiModule } from "src/app/services/ticket-api";
-import { TicketListComponent } from "./ticket-list/ticket-list.component";
+import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { TicketApiModule } from "src/app/services/ticket-api";
 import { PageHeaderComponent } from "src/app/shared/components/page-header/page-header.component";
+import { TicketFormComponent } from "./ticket-form/ticket-form.component";
+import { TicketListComponent } from "./ticket-list/ticket-list.component";
 import { TicketRoutingModule } from "./ticket-rounting.module";
 
 @NgModule({
     imports: [
         CommonModule,
-        TicketApiModule,
         RouterModule,
+        ReactiveFormsModule,
+        TicketApiModule,
         TicketRoutingModule,
         PageHeaderComponent,
     ],
     declarations: [
         TicketListComponent,
+        TicketFormComponent,
     ],
 })
 export class TicketModule { }
