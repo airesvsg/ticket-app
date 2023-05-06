ad2497b2e56c0d36626e6b37ccf14d67ad4c090b
Aires Gonçalves <a.silva.goncalves@avanade.com>
2023-05-05 21:32:27 -0300

    not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { authGuard } from './shared/guards/auth/auth.guard';
 const routes: Routes = [
   { path: '', pathMatch: 'full', loadChildren: () => import('./pages/login/login.routes') },
   { path: 'tickets', canActivate: [authGuard], loadChildren: () => import('./pages/ticket/ticket.module').then(m => m.TicketModule) },
+  { path: '**', loadChildren: () => import('./pages/not-found/not-found.routes') }
 ];
 
 @NgModule({
